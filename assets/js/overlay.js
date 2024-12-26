ac7c8d0e59ed5e1ef73f9e6e20ba37143dfb9428
haha <137051406+sad4whattt@users.noreply.github.com>
2024-12-26 14:07:49 -0500

    123

--- assets/js/overlay.js
+++
@@ -1,8 +0,0 @@
-//CREDITS TO https://github.com/Euro-pol
-$(document).ready(function(){ //CREDITS TO https://github.com/Euro-pol
-    $("#overlay").click(function(){ //CREDITS TO https://github.com/Euro-pol
-        $("#overlay").fadeOut(1000); //CREDITS TO https://github.com/Euro-pol
-        $("#audio")[0].play(); //CREDITS TO https://github.com/Euro-pol
-    }); //CREDITS TO https://github.com/Euro-pol
-}); //CREDITS TO https://github.com/Euro-pol
-//CREDITS TO https://github.com/Euro-pol
